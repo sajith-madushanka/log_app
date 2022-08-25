42c394f0d7ad08c9c308bcebda9b6b2d65c895e6
sajith-madushanka <lankathilakarokz@gmail.com>
2022-08-25 16:56:55 +0530

    initial commit

---
+++ src/Controller/LogsController.php
@@ -0,0 +1,66 @@
+<?php
+namespace Src\Controller;
+
+use Src\TableGateways\LogsGateway;
+
+class LogsController {
+
+    private $db;
+    private $requestMethod;
+    private $userId;
+
+    private $LogsGateway;
+
+    public function __construct($db, $requestMethod)
+    {
+        $this->db = $db;
+        $this->requestMethod = $requestMethod;
+
+        $this->LogsGateway = new LogsGateway($db);
+    }
+
+    public function processRequest()
+    {
+        switch ($this->requestMethod) {
+            case 'GET':
+                $response = $this->getAllLogs();
+                break;
+            case 'POST':
+                $response = $this->createLogFromRequest();
+                break;
+            default:
+                $response = $this->notFoundResponse();
+                break;
+        }
+        header($response['status_code_header']);
+        if ($response['body']) {
+            echo $response['body'];
+        }
+    }
+
+    private function getAllLogs()
+    {
+        
+        $result = $this->LogsGateway->findAll();
+        $response['status_code_header'] = 'HTTP/1.1 200 OK';
+        $response['body'] = json_encode($result);
+        return $response;
+    }
+
+    private function createLogFromRequest()
+    {
+        // return $_REQUEST['user_id'];
+        //$input = (array) json_decode(file_get_contents('php://input'), TRUE);
+        $this->LogsGateway->insert($_REQUEST['user_id'],$_REQUEST['status_code'],$_REQUEST['response'],$_REQUEST['payload'],$_REQUEST['route'],$_REQUEST['date_time']);
+        $response['status_code_header'] = 'HTTP/1.1 201 Created';
+        $response['body'] = 'Inserted the record';
+        return $response;
+    }
+
+    private function notFoundResponse()
+    {
+        $response['status_code_header'] = 'HTTP/1.1 404 Not Found';
+        $response['body'] = null;
+        return $response;
+    }
+}
---
+++ src/TableGateways/LogsGateway.php
@@ -0,0 +1,55 @@
+<?php
+namespace Src\TableGateways;
+
+class LogsGateway {
+
+    private $db = null;
+
+    public function __construct($db)
+    {
+        $this->db = $db;
+    }
+
+    public function findAll()
+    {
+        $statement = "
+            SELECT 
+                id, user_id, status_code,status
+            FROM
+                log_data;
+        ";
+
+        try {
+            $statement = $this->db->query($statement);
+            $result = $statement->fetchAll(\PDO::FETCH_ASSOC);
+            return $result;
+        } catch (\PDOException $e) {
+            exit($e->getMessage());
+        }
+    }
+
+    public function insert($user_id,$status_code,$response,$payload,$route,$date_time)
+    {
+        $statement = "
+            INSERT INTO log_data 
+                (user_id, status_code,response,payload,route,date_time)
+            VALUES
+                (:user_id, :status_code, :response, :payload, :route, :date_time);
+        ";
+
+        try {
+            $statement = $this->db->prepare($statement);
+            $statement->execute(array(
+                'user_id' => $user_id,
+                'status_code'  => $status_code,
+                'response'  => $response,
+                'payload' => $payload,
+                'route' => $route,
+                'date_time' => $date_time
+            ));
+            return $statement->rowCount();
+        } catch (\PDOException $e) {
+            exit($e->getMessage());
+        }    
+    }
+}
